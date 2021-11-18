c1de3b7755a1b7828e8dd293f510c555b876b638
Matt D <mattdee@gmail.com>
2021-11-18 15:17:07 +0100

    Add files via upload

---
+++ mssqlDocker.sh
@@ -0,0 +1,244 @@
+#!/bin/bash
+#
+#
+   #===================================================================================
+   #
+   #         FILE: mssqlDocker.sh
+   #
+   #        USAGE: run it
+   #
+   #  DESCRIPTION:
+   #      OPTIONS:  
+   # REQUIREMENTS: 
+   #       AUTHOR: Matt D
+   #      CREATED: 11.18.2021
+   #      UPDATED: 11.18.2021
+   #      VERSION: 1.0
+   #
+   #
+   #
+   #
+   #
+   #
+   #===================================================================================
+
+
+function startUp()
+{
+    clear screen
+    echo "##########################################################"
+    echo "# This will manage your MSSQL Database Docker container  #"
+    echo "##########################################################"
+
+    echo
+    echo
+    echo 
+
+    echo "################################################"
+    echo "#                                              #"
+    echo "#    What would you like to do ?               #"
+    echo "#                                              #"
+    echo "#          1 ==   Start MSSQL  docker image    #"
+    echo "#                                              #"
+    echo "#          2 ==   Stop MSSQL docker image      #"
+    echo "#                                              #"
+    echo "#          3 ==   Bash access                  #"            
+    echo "#                                              #"
+    echo "#          4 ==   SQLCMD                       #"
+    echo "#                                              #"
+    echo "#          5 ==   Do NOTHING                   #"
+    echo "#                                              #"
+    echo "################################################"
+    echo 
+    echo "Please enter in your choice:> "
+    read whatwhat
+
+#   if [ $whatwhat -gt 9 ]
+#       then
+#       echo "Please enter a valid choice"
+#       sleep 3
+#       startUp
+#   fi
+    
+}
+
+function doNothing()
+{
+    echo "################################################"
+    echo "You don't want to do nothing...lazy..."
+    echo "So...you want to quit...yes? "
+    echo "Enter yes or no"
+    echo "################################################"
+    read doWhat
+    if [[ $doWhat = yes ]]; then
+        echo "Yes"
+        echo "Bye! ¯\_(ツ)_/¯ " 
+        exit 1
+    else
+        echo "No"
+        startUp
+    fi
+    
+}
+
+function countDown()
+{
+    row=2
+    col=2
+    
+    clear 
+    msg="Please wait for MSSQL to start ...${1}..."
+    tput cup $row $col
+    echo -n "$msg"
+    l=${#msg}
+    l=$(( l+$col ))
+    for i in {30..1}
+        do
+            tput cup $row $l
+            echo -n "$i"
+            sleep 1
+         done
+    startUp
+}
+
+function badChoice()
+{
+    echo "Invalid choice, please try again..."
+    sleep 5
+    startUp
+}
+
+function checkDocker()
+{
+    # open Docker, only if is not running...super hacky
+    if (! docker stats --no-stream ); then
+        open /Applications/Docker.app
+    while (! docker stats --no-stream ); do
+        echo "Waiting for Docker to launch..."
+        sleep 1
+    done
+    fi
+}
+
+function copyFile()
+{
+    checkDocker
+    export mssqlRunning=$(docker ps --no-trunc --format "table {{.ID}}\t {{.Names}}\t" | grep -i MSSQL_DB_Container  | awk '{print $2}' )
+    echo "Please enter the ABSOLUTE PATH to the file you want copied: "
+    read thePath
+    echo "Please enter the FILE NAME you want copied: "
+    read theFile
+    echo "Copying info: " $thePath/$theFile
+    docker cp $thePath/$theFile $mssqlRunning:/tmp
+
+}
+
+
+
+function startMssql() # start or restart the container named MSSQL_DB_Container
+{   
+    checkDocker
+    # check to see if MSSQL_DB_Container is running and if running exit
+    export mssqlRunning=$(docker ps --no-trunc --format "table {{.ID}}\t {{.Names}}\t" | grep -i MSSQL_DB_Container  | awk '{print $2}' )
+    export mssqlPresent=$(docker container ls -a --no-trunc --format "table {{.ID}}\t {{.Names}}\t" | grep -i MSSQL_DB_Container  | awk '{print $2}')
+
+    if [ "$mssqlRunning" == "MSSQL_DB_Container" ]; then
+        echo "MSSQL docker container is running, please select other option."
+        sleep 5
+        startUp
+    elif [ "$mssqlPresent" == "MSSQL_DB_Container" ]; then
+        echo "MSSQL docker container found, restarting..."
+        docker restart $mssqlPresent
+        countDown
+    else
+        echo "No MSSQL docker image found, provisioning..."
+        docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Am@zon2021!' -e 'MSSQL_PID=Express' --name MSSQL_DB_Container --network="bridge" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest-ubuntu 
+        export runningAs=$(docker ps --no-trunc --format "table {{.ID}}\t {{.Names}}\t" | grep -i MSSQL_DB_Container  | awk '{print $2}' )
+        echo "MSSQL is running as: "$runningAs
+        echo "Please be patient as it takes time for the container to start..."
+        countDown
+    fi
+
+}
+
+
+function stopMssql()
+{
+    checkDocker
+    export stopMssql=$(docker ps --no-trunc --format "table {{.ID}}\t {{.Names}}\t" | grep -i MSSQL_DB_Container  | awk '{print $1}' )
+    echo $stopMssql
+
+    for i in $stopMssql
+    do
+        echo $i
+        echo "Stopping container: " $i
+        docker stop $i
+    done
+
+    cleanVolumes
+
+}
+
+
+function cleanVolumes()
+{
+    docker volume prune -f 
+}
+
+
+function bashAccess()
+{
+    checkDocker
+    export mssqlImage=$(docker ps --no-trunc --format "table {{.ID}}\t {{.Names}}\t" | grep -i MSSQL_DB_Container  | awk '{print $1}' )
+    docker exec -it $mssqlImage /bin/bash
+}
+
+function rootAccess()
+{
+    checkDocker
+    #export mssqlImage=$(docker ps --format "table {{.Image}}\t{{.Ports}}\t{{.Names}}"| grep -i oracle  | awk '{print $4}')
+    # this works by greping the known oracle database port
+    export mssqlImage=$(docker ps --no-trunc --format "table {{.ID}}\t {{.Names}}\t" | grep -i MSSQL_DB_Container  | awk '{print $1}' )
+    docker exec -it -u 0 $mssqlImage /bin/bash
+}
+
+
+function sqlCmd()
+{
+    checkDocker
+    export mssqlImage=$(docker ps --no-trunc --format "table {{.ID}}\t {{.Names}}\t" | grep -i MSSQL_DB_Container  | awk '{print $1}' )
+    docker exec -it $mssqlImage /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Am@zon2021!
+}
+
+
+
+
+# Let's go to work
+startUp
+case $whatwhat in
+    1) 
+        startMssql
+        ;;
+    2) 
+        stopMssql
+        ;;
+    3)
+        bashAccess
+        ;;   
+    4)
+        sqlCmd
+        ;;
+    5)
+        doNothing
+        ;;
+    6)  # secret menu like in-n-out ;-) 
+        cleanVolumes
+        ;;
+    7)
+        copyFile
+        ;;
+    *) 
+        badChoice
+esac
+
+
